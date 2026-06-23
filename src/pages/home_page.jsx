ee8c59787d005793bbe834cefec5f7e97ebae161
Hrithik <hrithiknonia66@gmail.com>
2026-06-23 21:02:54 +0530

    section 7 done

--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -213,7 +213,86 @@ export default function Home() {
           </section>
 
           {/* section 7 */}
+          <section className="mt-10">
+            <div className="text-center py-25 bg-white text-3xl font-semibold mb-3">
+              Brands
+            </div>
 
+            {/* main content */}
+            <section className="w-full bg-[#7B1FFF] py-16 px-6 md:px-10">
+              <div className="max-w-5xl mx-auto text-white">
+                {/* Heading */}
+                <div className="text-center">
+                  <h2 className="text-4xl font-bold">Newsletter</h2>
+                  <p className="mt-4 text-lg text-white/90">
+                    Sign up to receive updates on new arrivals and special
+                    offers
+                  </p>
+                </div>
+
+                {/* Form */}
+                <form className="mt-12">
+                  <label className="block text-lg mb-4">
+                    Email <span>*</span>
+                  </label>
+
+                  <div className="flex flex-col lg:flex-row gap-6">
+                    <input
+                      type="email"
+                      placeholder=""
+                      className="
+                      flex-1
+                      h-14
+                      rounded-full
+                      border border-white
+                      bg-transparent
+                      px-6
+                      outline-none
+                      text-white
+                      placeholder:text-white/60
+                    "
+                    />
+
+                    <button
+                      type="submit"
+                      className="
+                      h-14
+                      px-12
+                      rounded-full
+                      bg-black
+                      text-white
+                      text-lg
+                      hover:bg-transparent
+                      border-black
+                      border
+                      transition
+                    "
+                    >
+                      Subscribe
+                    </button>
+                  </div>
+
+                  {/* Checkbox */}
+                  <label className="mt-8 flex items-center gap-4 cursor-pointer">
+                    <input
+                      type="checkbox"
+                      className="
+                h-5
+                w-5
+                border
+                border-white
+                bg-transparent
+                accent-white
+              "
+                    />
+                    <span className="text-xl">
+                      Yes, subscribe me to your newsletter. *
+                    </span>
+                  </label>
+                </form>
+              </div>
+            </section>
+          </section>
           {/* section 8 */}
         </section>
       </section>
